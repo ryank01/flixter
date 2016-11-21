4fc289a8a298c7bc41d37f518d0a8752f7b67603
Ryan <ryankawall01@gmail.com>
2016-11-21 18:22:20 +0000

    Auth enrolled user before allowing to view lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
   def show
   end
 
   private
   helper_method :current_lesson
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      redirect_to courses_path, alert: 'Please enroll in course to view lessons'
+    end
+  end
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
