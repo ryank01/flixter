dfc988677d475a8c24f16c2bead6e7eb47513fd0
Ryan <ryankawall01@gmail.com>
2016-12-04 06:59:23 +0000

    Added row ordering to sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -11,6 +11,11 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_course.update_attributes(section_params)
+    render text: 'updated!'
+  end
+
   private
 
   def require_authorized_for_current_course
@@ -24,7 +29,15 @@ class Instructor::SectionsController < ApplicationController
     @current_course ||= Course.find(params[:course_id])
   end
 
+  def current_course
+  if params[:course_id]
+    @current_course ||= Course.find(params[:course_id])
+  else
+    current_section.course
+  end
+end
+
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title,:row_order_position)
   end
 end
